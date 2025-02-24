3a68d6a144a7e04a72036a373c6b22123b768ff8
jlakvidu <jlakvidu@gmail.com>
2025-02-24 09:58:26 +0530

    second commit

--- frontend/src/components/TheWelcome.vue
+++ frontend/src/components/TheWelcome.vue
@@ -17,7 +17,7 @@ const openReadmeInEditor = () => fetch('/__open-in-editor?file=README.md')
     <template #heading>Documentation</template>
 
     Vue’s
-    <a href="https://vuejs.org/" target="_blank" rel="noopener">official documentation</a>
+    <a href="https://vuejs.org/" target="_blank" rel="noopener">official Ne bng documentation</a>
     provides you with all information you need to get started.
   </WelcomeItem>
 
